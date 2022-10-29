89a858b749e6d0b070a777a40ea14a8c6da873c0
Nikita <Nikita@TheVer4.ru>
2022-10-29 15:45:01 +0500

    Final in feature

---
+++ MyNewGitProject/OddNumbers.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MyNewGitProject {
+    public class OddNumbers {
+        public readonly List<int> numbers;
+
+        public OddNumbers(int max) {
+            numbers = new List<int>();
+            for (int i = 1; i < max; i+=2) {
+                numbers.Add(i);
+            }
+        }
+
+        public override string ToString() {
+            return string.Join(", ", numbers);
+        }
+    }
+}

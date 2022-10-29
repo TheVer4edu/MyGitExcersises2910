2685b0b8d66c6b96b4d556fc1b418de97121ca45
Nikita <Nikita@TheVer4.ru>
2022-10-29 13:47:18 +0500

    Very signful commit

--- MyNewGitProject/Program.cs
+++ MyNewGitProject/Program.cs
@@ -3,7 +3,11 @@
 namespace MyNewGitProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string str = "Hello World!";
+            foreach (char c in str) {
+                Console.Write(c);
+            }
+            Console.WriteLine();
         }
     }
 }
